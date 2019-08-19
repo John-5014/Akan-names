40fe0ba7c1f84c2dfe508132dc4b61690010f311
John-5014 <kaiserjohn52@gmail.com>
2019-08-19 11:59:13 +0300

    change existing codes to new ones for functionality

--- custom.js
+++ custom.js
@@ -1,83 +1,108 @@
 function validateform(){
-  var day = document.getElementById("day").value;
-  var month = document.getElementById("month").value;
-  var year = document.getElementById("year").value;
-  var centuary = document.getElementById("centuary").value;
-  var gender = document.getElementsByName("gender");
- 
-
-  if (day == "" || day == null) {
-    alert("Day is required");
-  } else {
-    if (!isNaN(day)) {
-      if (day <= 0 || day > 31) {
-        alert("day is invalid");
+  var day = Number(document.getElementById("day").value);
+  var month = Number(document.getElementById("month").value);
+  var year = Number(document.getElementById("year").value);
+  var century = Number(document.getElementById("centuary").value);
+  var genderSelected = document.getElementsByName("gender");
+  function genderPicked() {
+      for (var gender of genderSelected) {
+          if (gender.checked){
+              return gender.value;
+          }
+      }
+  }
+  var genderValue = genderPicked();
+  // alert(genderValue);
+  function dayValid () {
+      if (day < 1 || day > 31) {
+          alert("Invalid day")
+          return false;
       } else {
-        alert(day);
+          return true;
       }
-    } else {
-      alert("Number required");
-    }
   }
-
-  if (month == "" || month == null) {
-    alert("month is required");
-  } else {
-    if (!isNaN(month)) {
-      if (month <= 0 || month > 12) {
-        alert("month is invalid");
+  function monthValid () {
+      if (month < 1 || month > 12) {
+          alert ("Invalid month");
+          return false;
       } else {
-        alert(month);
+          return true;
       }
-    } else {
-      alert("Number required");
-    }
   }
-
-  if (centuary == "" || centuary == null) {
-    alert("century is required");
+  var isDayValid = dayValid();
+  var isMonthValid = monthValid();
+  var dayIndex = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7);
+var akanName;
+  if (genderValue === "male") {
+      switch(dayIndex) {
+              case 0:
+          akanName = "Kwasi";
+          alert("Born on Sunday : Kwasi");
+          break;
+          case 1:
+          akanName = "Kwadwo";
+          alert("Born on Monday : Kwadwo");
+          break;
+          case 2:
+          akanName = "Kwabena";
+          alert("Born on Tuesday : Kwabena");
+          break;
+          case 3:
+          akanName = "Kwaku";
+          alert("Born on Wednesday : Kwaku");
+          break;
+          case 4:
+          akanName = "Yaw";
+          alert("Born on Thursday : Yaw");
+          break;
+          case 5:
+          akanName = "Kofi";
+          alert("Born on Friday : Kofi");
+          break;
+          case 6:
+          akanName = "Kwame";
+          alert("Born on Saturday : Kwame");
+          break;
+          default:
+          akanName = "Please input the correct details!"
+          alert("Please input the correct details!");
+      }
+  } else if (genderValue = "female") {
+      switch(dayIndex) {
+              case 0:
+              akanName = "Akosua";
+              alert("Born on Sunday : Akosua");
+              break;
+              case 1:
+              akanName = "Adwoa";
+              alert("Born on Monday : Adwoa");
+              break;
+              case 2:
+              akanName = "Abenaa";
+              alert("Born on Tuesday : Abenaa");
+              break;
+              case 3:
+              akanName = "Akua";
+              alert("Born on Wednesday : Akua");
+              break;
+              case 4:
+              akanName = "Yaa";
+              alert("Born on Thursday : Yaa");
+              break;
+              case 5:
+              akanName = "Afua";
+              alert("Born on Friday : Afua");
+              break;
+              case 6:
+              akanName = "Ama";
+              alert(" Born on Saturday : Ama");
+              break;
+              default:
+              akanName = "Please input the correct details!"
+              alert("Please input the correct details!");
+          }
   } else {
-    if (!isNaN(centuary)) {
-      
-        alert("centuary is invalid");
-      } else {
-        alert(centuary);
-      }
-    
+              alert("Please try again with the correct data.");
   }
-
-
-  if (year == "" || year == null) {
-    alert("year is required");
-  } else {
-    if (!isNaN(year)) {
-      if (year <= 0 || year > 2045) {
-        alert("year is invalid");
-      } else {
-        alert(year);
-      }
-    } else {
-      alert("Number required");
-    }
-  }
-
-  if (gender[0].checked === false && gender[1].checked === false) {
-    alert("select gender");
-  } else {
-    for(i = 0; i < gender.length; i++){
-      if(gender[i].checked) {
-        alert(gender[i].value);
-      }
-    }
-  }
-
- 
-
-
-
-
-
-
-
-
 }
+
